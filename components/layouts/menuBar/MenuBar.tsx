ff5eba065eb40ce9eadc6cd8e95dc05a6a3cea95
YamazoeShintaro <ys37799665@gmail.com>
2024-06-17 20:27:02 +0900

    💄 MenuBarのレイアウトを修正

--- components/layouts/menuBar/MenuBar.tsx
+++ components/layouts/menuBar/MenuBar.tsx
@@ -1,32 +1,32 @@
 "use client";
 
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHouse } from "@fortawesome/free-solid-svg-icons"
-import { CurrentPageProps } from "@/components/types/Props";
 import Link from "next/link";
 
-const MenuBar = ({ currentPage }: CurrentPageProps) => {
+import { CurrentPageProps } from "@/components/types/Props";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function MenuBar({ currentPage }: CurrentPageProps) {
     const navItems = [
         { path: "/", content: <FontAwesomeIcon icon={ faHouse } className="text-xl"/>, pageName: "home", border: true },
-        { path: "/js-ts-page", content: "JS / TS", pageName: "JS/TS", border: true },
+        { path: "/js-ts-page", content: "JS / TS", pageName: "JavaScript / TypeScript", border: true },
         { path: "/react-page", content: "React", pageName: "React", border: true },
         { path: "/nextjs-page", content: "Next.js", pageName: "Next.js", border: true },
         { path: "/nodejs-page", content: "Node.js", pageName: "Node.js", border: true },
         { path: "/mysql-page", content: "MySQL", pageName: "MySQL", border: true },
-        { path: "/others-page", content: "その他", pageName: "others", border: false }
+        { path: "/others-page", content: "その他", pageName: "others", border: false },
     ];
 
     return (
-        <div className="bg-white px-20 py-2 flex justify-between">
+        <div className="bg-white px-10 py-2 flex flex-wrap justify-center">
             {navItems.map((item, index) => (
                 <>
                     <Link
                         key={index}
                         href={item.path}
                         className={item.pageName === currentPage
-                            ? "text-ocean-blue font-medium border-b border-solid border-ocean-blue text-lg px-5 py-2"
-                            : "text-lg font-light px-5 py-2 duration-300 hover:opacity-50 hover:border-solid hover:border-b-2"}
+                            ? "text-ocean-blue font-medium border-b border-solid border-ocean-blue text-lg mx-5 px-5 py-2"
+                            : "text-lg font-light mx-5 px-5 py-2 duration-300 hover:opacity-50 hover:border-solid hover:border-b-2"}
                     >
                         {item.content}
                     </Link>
@@ -36,5 +36,3 @@ const MenuBar = ({ currentPage }: CurrentPageProps) => {
         </div>
     );
 };
-
-export default MenuBar;
