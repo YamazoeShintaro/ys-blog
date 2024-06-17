149693d6333399ae5bc8307b688c4131a5dc401d
YamazoeShintaro <ys37799665@gmail.com>
2024-06-17 18:41:48 +0900

    💄 見た目を整えた

--- components/layouts/header/Header.tsx
+++ components/layouts/header/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
         <div className="flex justify-center items-center h-40 bg-ocean-blue">
             <div className="flex relative pl-14 pr-9">
                 <FontAwesomeIcon icon={faCat} className="text-shine-muscat text-5xl absolute bottom-1 left-0"/>
-                <Link href={"./"} className="text-6xl font-thin italic text-shine-muscat">_Y S _ b l o g _</Link>
+                <p className="text-6xl font-thin italic text-shine-muscat">_Y S _ b l o g _</p>
                 <FontAwesomeIcon icon={faPencil} className="text-shine-muscat text-4xl absolute bottom-1 right-0"/>
             </div>
         </div>
--- components/layouts/profile/Profile.tsx
+++ components/layouts/profile/Profile.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 function Profile() {
     return (
-        <div className="p-8 bg-white">
-            <h2 className="text-lg border-b border-solid px-3 pb-2">プロフィール</h2>
-            <div className="flex flex-col items-center mx-5 py-8">
+        <div className="px-6 py-8 bg-white">
+            <p className="text-xl border-b border-solid px-3 pb-2">プロフィール</p>
+            <div className="flex flex-col items-center mx-5 py-10">
                 <Image
                     className="rounded-full"
                     src="/profile_cat.png"
@@ -13,11 +13,11 @@ function Profile() {
                     height={160}
                     alt="Picture of the author"
                 />
-                <h3 className="text-xl pt-5">YS</h3>
+                <h3 className="text-xl pt-3 pb-1">YS</h3>
+                <div className="text-sm">フロントエンドエンジニア</div>
             </div>
-            <p className="mx-5">駆け出しフロントエンドエンジニア</p><br/>
-            <p className="mx-5">猫が大好き</p><br/>
-            <p className="mx-5">JavaScript / TypeScript / React / Next.js / MySQL</p>
+            <p className="mx-5 border-b border-solid">猫が大好き</p>
+            <p className="mx-5 pt-2 border-b border-solid ">JavaScript / TypeScript / React / Next.js / MySQL</p>
         </div>
     );
 };
