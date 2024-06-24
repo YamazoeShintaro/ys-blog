14e2c002ea051a4aa350217b87e14c5fbe8551da
YamazoeShintaro <ys37799665@gmail.com>
2024-06-25 02:07:29 +0900

    ✨ SNSへのリンクを追加

--- components/layouts/profile/Profile.tsx
+++ components/layouts/profile/Profile.tsx
@@ -1,10 +1,12 @@
+import { faSquareGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 
 export default function Profile() {
     return (
         <div className="px-6 py-8">
             <p className="text-lg border-b border-solid border-shine-muscat px-3 pb-2">プロフィール</p>
-            <div className="flex flex-col items-center py-10">
+            <div className="flex flex-col items-center pt-10 pb-6">
                 <div className="image-container">
                     <Image
                         className="rounded-full"
@@ -19,6 +21,19 @@ export default function Profile() {
             </div>
             <p className="mx-2 px-2 border-b border-solid">猫が大好き</p>
             <p className="mx-2 px-2 mt-4 border-b border-solid">JavaScript / TypeScript / React / Next.js / MySQL</p>
+            <div className="flex mx-2 py-2 mt-4 border-b border-solid">
+                    <a href="https://zenn.dev/ys37799665" target="_blank" className="px-2 hover:opacity-50">
+                        <Image
+                            src="/zenn_icon.svg"
+                            alt="zenn_icon"
+                            width={28}
+                            height={28}
+                        />
+                    </a>
+                    <a href="https://github.com/YamazoeShintaro" target="_blank" className="px-2 hover:opacity-50">
+                        <FontAwesomeIcon icon={faSquareGithub} className="text-3xl"/>
+                    </a>
+                </div>
         </div>
     );
 };
