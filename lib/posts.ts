8b08cdf3d3e3954bae5c8712b07c95f78a343347
YamazoeShintaro <ys37799665@gmail.com>
2024-06-26 17:53:06 +0900

    🔥 不要なコードを削除

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,35 +1,6 @@
 import { ArticleResponse } from "@/components/types/Article";
 import { fetchGithubMakeArticle, fetchGithubRepo } from "@/lib/utility/getArticle";
 
-// export async function getAllSortedPostsData() {
-//   const zennArticles: ArticleResponse[] = await fetchGithubRepo("https://api.github.com/repos/YamazoeShintaro/zenn-articles/contents/articles");
-
-//   const datas = await (async (zennArticles) => {
-//     if (zennArticles) {
-//       return await Promise.all(zennArticles.map( async (article: ArticleResponse) => {
-//         return fetchGithubMakeArticle("https://api.github.com/repos/YamazoeShintaro/zenn-articles/contents/articles/", article.name);
-//       }));
-//     }
-//   })(zennArticles);
-
-//   const removeFalsyDatas = datas?.filter(Boolean);
-
-//   console.log(removeFalsyDatas);
-
-//   const allSortedPostsData = removeFalsyDatas?.sort((a, b) => {
-//     if (a!.date === b!.date){
-//       return 0;
-//     };
-//     if (a!.date < b!.date) {
-//       return 1;
-//     } else {
-//       return -1;
-//     };
-//   });
-
-//   return allSortedPostsData;
-// };
-
 export async function getAllSortedPostsData() {
   const zennArticles = await fetchGithubRepo("https://api.github.com/repos/YamazoeShintaro/zenn-articles/contents/articles");
 
